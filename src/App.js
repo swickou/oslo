89a45517a8749efd07cdf88b0b26fdda18926f30
cedric <cedric.greufeille@gmail.com>
2021-05-14 23:08:32 +0200

    add box

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import TableauMeteo from "./components/TableauMeteo.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello Word !
-        </a>
-      </header>
+        <TableauMeteo></TableauMeteo>
     </div>
   );
 }
---
+++ src/components/TableauMeteo.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import Box from '@material-ui/core/Box';
+import { createMuiTheme, withStyles, makeStyles, ThemeProvider } from '@material-ui/core/styles';
+
+const defaultProps = {
+    bgcolor: 'background.paper',
+    m: 1,
+    style: { width: '5rem', height: '5rem' },
+    borderColor: 'text.primary',
+};
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        backgroundColor: 'red',
+        color: props => props.color,
+    },
+    b1: {
+        backgroundColor : 'greenyellow'
+    },
+    b2: {
+        backgroundColor : 'red'
+    }
+
+}));
+
+export default function TableauMeteo() {
+    const classes = useStyles();
+    return (
+        <>
+        <Box display="flex" justifyContent="center">
+            <Box border={1} {...defaultProps} className={classes.b1} />
+            <Box border={1} {...defaultProps} className={classes.b2} />
+        </Box>
+        </>
+    );
+}
